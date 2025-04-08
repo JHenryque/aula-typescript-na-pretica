f1f4bbf83767b613935eb5c53be79bdef65583e5
JHenrique <henryque.pc.game@gmail.com>
2025-04-08 13:04:30 -0300

    definição da interface do estudante

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,38 @@
-import data from './data.js' ;
+import data from "./data.js";
 
 const students = JSON.parse(data);
 console.log(students);
 
+interface Student {
+  id: string;
+  dateAdmission: string;
+  firstName: string;
+  lastName: string;
+  birthYear: string;
+  focusArea?: string | string[];
+  dateRegistrationSuspended?: string;
+}
+
 function addRow(table) {
-    
-    let tr = table.querySelector("tbody").insertRow();
-    const name = tr.insertCell();
-    name.appendChild(document.createTextNode("Elena Garcia"));
+  let tr = table.querySelector("tbody").insertRow();
+  const name = tr.insertCell();
+  name.appendChild(document.createTextNode("Elena Garcia"));
 
-    const age = tr.insertCell();
-    age.appendChild(document.createTextNode("33"));
+  const age = tr.insertCell();
+  age.appendChild(document.createTextNode("33"));
 
-    const majors = tr.insertCell();
-    majors.appendChild(document.createTextNode("Space Engineering, Civil Engineering"));
+  const majors = tr.insertCell();
+  majors.appendChild(
+    document.createTextNode("Space Engineering, Civil Engineering")
+  );
 
-    const status = tr.insertCell();
-    status.appendChild(document.createTextNode("Active"));
+  const status = tr.insertCell();
+  status.appendChild(document.createTextNode("Active"));
 }
 
 // select HTML table
 function selectTable() {
-    return document.querySelector("#students-table");
+  return document.querySelector("#students-table");
 }
 
 // add a row
